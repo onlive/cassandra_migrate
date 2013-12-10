067a35779aad012afb33ff26acae38667ada99f3
Noah Gibbs <noah.gibbs@onlive.com>
2013-12-10 13:29:39 -0800

    Finished first draft. AN-1063

--- lib/cassandra_migrate.rb
+++ lib/cassandra_migrate.rb
@@ -1,6 +1,11 @@
+# Copyright (C) 2013 OL2, inc.  See LICENSE.txt for details.
+
 require "cassandra_migrate/version"
 
 require "cql"
+require "erubis"
+
+require "digest/sha1"
 
 module CassandraMigrate
   attr :host
@@ -13,12 +18,6 @@ module CassandraMigrate
 
     @cassandra_client = Cql::Client.connect(:hosts => host, :port => port)
     @cassandra_client.use(keyspace) if keyspace
-  end
-
-  def execute_cql_file(file)
-    cql = File.read file
-
-    cql_client.execute cql
   end
 
   def execute_cql(cql)
@@ -42,7 +41,7 @@ module CassandraMigrate
       end
       migration[:desc] = desc
 
-      migration[:actions][action] = {
+      migration[:actions][action.to_sym] = {
         file: file
       }
     end
@@ -56,14 +55,23 @@ module CassandraMigrate
     if ks.empty?
       peers = execute_cql "SELECT peer FROM system.peers;"
 
-      replication = [3, peers.size + 1].max
+      @replication = [3, peers.size + 1].max
       execute_cql <<-MIGRATION
         CREATE KEYSPACE "schema" WITH REPLICATION =
-          { 'class' : 'SimpleStrategy', 'replication_factor' : #{replication} };
+          { 'class' : 'SimpleStrategy', 'replication_factor' : #{@replication} };
       MIGRATION
     end
 
-
+    cf = execute_cql "SELECT columnfamily_name FROM system.schema_columnfamilies WHERE columnfamily_name = 'migrations' AND keyspace_name = 'schema';"
+    if cf.empty?
+      execute_cql <<-MIGRATION
+        CREATE TABLE "migrations" (
+          "date_string" varchar,
+          "up_filename" varchar,
+          "sha1" varchar,
+        PRIMARY KEY ("date_string", "up_filename"));
+      MIGRATION
+    end
   end
 
   def migrations_completed(refresh = false)
@@ -73,23 +81,93 @@ module CassandraMigrate
 
     @migrations_completed = {}
 
-    # TODO: finish
+    migrations = execute_cql "SELECT * FROM schema.migrations;"
+    STDERR.puts "Migrations: #{migrations.inspect}"
+    migrations.each do |migration|
+      @migrations_completed[migration["date_string"]] = migration.to_hash
+    end
+
     @migrations_completed
   end
 
+  def sha1(path)
+    Digest::SHA1.hexdigest File.read path
+  end
+
+  def execute_migration_file(path)
+    ensure_schema_keyspace_exists
+
+    STDERR.puts "Executing migration file: #{path.inspect}"
+
+    components = path.split(".")
+    components.shift   # Take just the extensions
+
+    content = File.read path
+
+    while components.size > 1
+      ext = components.shift
+
+      if ext == "erb" || ext == "erubis"
+        eruby = Erubis::Eruby.new content
+        content = eruby.result :replication => @replication
+      else
+        raise "Unknown intermediate extension in path #{path.inspect}: #{ext.inspect}!"
+      end
+    end
+
+    final_type = components.first
+    if ["cql", "cqlsh"].include?(final_type)
+      execute_cql content
+    elsif ["erb", "erubis"].include?(final_type)
+      raise "Can't use erb as the final extension in path #{path.inspect}!"
+    else
+      raise "Unknown extension #{final_type.inspect} in path #{path.inspect}!"
+    end
+  end
+
   def up(date_str, options = {})
+    raise "Can't apply migration #{date_str} that already happened!" if @migrations_completed[date_str]
+    raise "Can't apply migration #{date_str} that has no migration files!" unless @migrations_in_dir[date_str]
+    raise "Can't apply migration #{date_str} with no up migration!" unless @migrations_in_dir[date_str][:actions][:up]
+
+    up_filename = @migrations_in_dir[date_str][:actions][:up][:file]
+    execute_migration_file up_filename
+    execute_cql "INSERT INTO schema.migrations (date_string, up_filename, sha1) VALUES ('#{date_str}', '#{up_filename}', '#{sha1 up_filename}')"
   end
 
   def down(date_str, options = {})
+    raise "Can't reverse migration #{date_str} that didn't happen!" unless @migrations_completed[date_str]
+    raise "Can't reverse migration #{date_str} that has no migration files!" unless @migrations_in_dir[date_str]
+    raise "Can't reverse migration #{date_str} with no down migration!" unless @migrations_in_dir[date_str][:actions][:down]
+
+    execute_migration_file @migrations_in_dir[date_str][:actions][:down][:file]
+    execute_cql "DELETE FROM schema.migrations WHERE date_string = '#{date_str}';"
   end
 
   def up_to(date_str, options = {})
+    uncompleted_dates = @migrations_in_dir.keys - @migrations_completed.keys
+
+    migrations_to_run = uncompleted_dates.select { |d| d <= date_str }
+
+    STDERR.puts "Run #{migrations_to_run.size} migrations, update to #{date_str}."
+    migrations_to_run.each { |m| up(m, options) }
   end
 
   def down_to(date_str, options = {})
+    migrations_to_run = @migrations_completed.keys.select { |d| d >= date_str }
+
+    STDERR.puts "Run #{migrations_to_run.size} migrations, roll back to #{date_str}."
+    migrations_to_run.each { |m| down(m, options) }
   end
 
   def go_to_target(date_str, options = {})
+    current_latest = @migrations_completed.keys.max
+
+    if date_str < current_latest
+      down_to date_str
+    else
+      up_to date_str
+    end
   end
 
 end
